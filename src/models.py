5d524ecd3529a356c1ea3a44510fb60438747155
María Ripoll Vives <mariaripoll95@icloud.com>
2023-08-14 16:52:20 +0000

    Generated the databasee for the Star Wars Project

--- src/models.py
+++ src/models.py
@@ -7,23 +7,51 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class Character(Base):
+    __tablename__ = 'character'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    secondname = Column(String(250), nullable=False)
+    location = Column(String(250), ForeignKey('location.id'), nullable=False)
+    gender = Column(String(250), nullable=False)
+    weapon = Column(String(250), ForeignKey('weapon.id'), nullable=False)
+    species = Column(String(250), ForeignKey('species.id'), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Species(Base):
+    __tablename__ = 'species'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    dimensions = Column(Integer, nullable=False)
+    location = Column(String(250), ForeignKey('location.id'), nullable=False)
+
+class Location(Base):
+    __tablename__ = 'location'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    terrain = Column(String(250), nullable=False)
+    weapon = Column(String(250), ForeignKey('weapon.id'), nullable=False)
+    species = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+
+class Weapon(Base):
+    __tablename__ = 'weapon'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    affiliation = Column(String(250), nullable=False)
+    location = Column(String(250), ForeignKey('location.id'), nullable=False)
+
+class Vehicle(Base):
+    __tablename__ = 'vehicle'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    dimensions = Column(Integer, nullable=False)
+    location = Column(String(250), ForeignKey('location.id'), nullable=False)
 
     def to_dict(self):
         return {}
